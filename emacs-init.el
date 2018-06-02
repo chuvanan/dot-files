bd0d7b8adf2d7803bbc8d3bcc2306562fe022461
AnChu <chuvanan.cva@gmail.com>
2018-06-02 17:11:45 +0700

    Remove duplicated function

--- emacs-init.el
+++ emacs-init.el
@@ -912,20 +912,6 @@
 
 (global-set-key (kbd "C-x C-d") 'counsel-goto-recent-directory)
 
-(defun counsel-goto-recent-directory ()
-  "Open recent directory with dired"
-  (interactive)
-  (unless recentf-mode (recentf-mode 1))
-  (let ((collection
-         (delete-dups
-          (append (mapcar 'file-name-directory recentf-list)
-                  ;; fasd history
-                  (if (executable-find "fasd")
-                      (split-string (shell-command-to-string "fasd -ld") "\n" t))))))
-    (ivy-read "directories:" collection :action 'dired)))
-
-(global-set-key (kbd "C-x C-d") 'counsel-goto-recent-directory)
-
 ;; -----------------------------------------------------------------------------
 ;; org-mode
 ;; -----------------------------------------------------------------------------
