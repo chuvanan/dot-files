7d16ec6709fa0e0079c5d3216ffb0e3bb8a3278b
AnChu <chuvanan.cva@gmail.com>
2018-08-26 07:49:24 +0700

    add config.fish file

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+
+# solarized terminal
+eval (dircolors ~/.dir_colors/dircolors | head -n 1 | sed 's/^LS_COLORS=/set -x LS_COLORS /;s/;$//')
+
+alias sau="sudo apt update"
+alias sag="sudo apt upgrade"
+alias sai="sudo apt install"
+alias srb="sudo reboot"
+alias saar="sudo apt autoremove"
+alias saac="sudo apt autoclean"
+alias sshn="sudo shutdown -h now"
+
+# Caps Lock as Ctrl
+alias scasc="setxkbmap -option ctrl:nocaps"
